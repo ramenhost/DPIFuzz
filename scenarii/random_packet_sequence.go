894bd4e257b01fdf6585596e5b94d2b2f7490dc3
piano-man <reen.gagan@gmail.com>
2020-03-26 02:02:25 +0100

    random packet sequence refined

--- scenarii/random_packet_sequence.go
+++ scenarii/random_packet_sequence.go
@@ -2,17 +2,26 @@ package scenarii
 
 import (
 	"fmt"
+	qt "github.com/QUIC-Tracker/quic-tracker"
 	"math/rand"
 	"strings"
 	"time"
-
-	qt "github.com/QUIC-Tracker/quic-tracker"
 )
 
-var li = [4]uint64{0x00, 0x01, 0x02, 0x03}
+var li = [2]uint64{0x00, 0x01}
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 type RandomPacketSequenceScenario struct {
 	AbstractScenario
+}
+
+func RandStringBytes(n int) []byte {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return b
 }
 
 func NewRandomPacketSequenceScenario() *RandomPacketSequenceScenario {
@@ -35,7 +44,7 @@ func (s *RandomPacketSequenceScenario) Run(conn *qt.Connection, trace *qt.Trace,
 	num_packets := rand.Intn(20)
 	arr := make([]uint64, num_packets)
 	for i := 0; i < num_packets; i++ {
-		index := rand.Intn(4)
+		index := rand.Intn(2)
 		arr[i] = li[index]
 	}
 
@@ -44,18 +53,24 @@ func (s *RandomPacketSequenceScenario) Run(conn *qt.Connection, trace *qt.Trace,
 	for i := 0; i < num_packets; i++ {
 		switch c := arr[i]; c {
 		case 0x00:
-			fmt.Println("VIOLAAAAAAAAAAAAA\n")
 			// payload := []byte(fmt.Sprintf("GET %s\r\n", preferredPath))
 			// pp3 := qt.NewProtectedPacket(conn)
 			// pp3.Frames = append(pp3.Frames, qt.NewStreamFrame(0, 0, payload, false))
 			// conn.DoSendPacket(pp3, qt.EncryptionLevel1RTT)
-			conn.DoSendPacket(qt.NewInitialPacket(conn), qt.EncryptionLevelInitial)
-			// case 0x01:
-			// conn.DoSendPacket(qt.NewHandshakePacket(conn), qt.EncryptionLevelHandshake)
-			// case 0x02:
-			// 	conn.DoSendPacket(qt.NewProtectedPacket(conn), qt.EncryptionLevel1RTT)
+			conn.DoSendPacket(conn.GetInitialPacket(), qt.EncryptionLevelInitial)
+		// case 0x01:
+		// 	conn.DoSendPacket(qt.NewHandshakePacket(conn), qt.EncryptionLevelHandshake)
+		case 0x01:
+			// payload := []byte(fmt.Sprintf("GET %s\r\n", preferredPath))
+			payload := RandStringBytes(rand.Intn(100))
+			pp3 := qt.NewProtectedPacket(conn)
+			pp3.Frames = append(pp3.Frames, qt.NewStreamFrame(0, 0, payload, false))
+			conn.DoSendPacket(pp3, qt.EncryptionLevel1RTT)
 			// case 0x03:
-			// 	conn.DoSendPacket(qt.NewZeroRTTProtectedPacket(conn), qt.EncryptionLevel0RTT)
+			// 	payload := []byte(fmt.Sprintf("GET %s\r\n", preferredPath))
+			// 	pp4 := qt.NewZeroRTTProtectedPacket(conn)
+			// 	pp4.Frames = append(pp4.Frames, qt.NewStreamFrame(0, 0, payload, false))
+			// 	conn.DoSendPacket(pp4, qt.EncryptionLevel0RTT)
 		}
 	}
 
