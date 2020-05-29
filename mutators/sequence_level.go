f624160e9167891b54b9f19655a4e82f135c5ff1
piano-man <reen.gagan@gmail.com>
2020-05-29 16:39:03 +0200

    production code: paper specific changes added

--- mutators/sequence_level.go
+++ mutators/sequence_level.go
@@ -1,11 +1,61 @@
 package mutators
 
 import (
+	// "fmt"
 	. "github.com/QUIC-Tracker/quic-tracker"
 )
 
 func SequenceLevelMutations(packetList []*ProtectedPacket) []*ProtectedPacket {
-	R.Shuffle(len(packetList), func(i, j int) { packetList[i], packetList[j] = packetList[i], packetList[i] }) //Modified Shuffle
+	R.Shuffle(len(packetList), func(i, j int) { packetList[i], packetList[j] = packetList[i], packetList[i] }) //Modified Shuffle(Shuffle+Drop+Duplicate)
 	// R.Shuffle(len(packetList), func(i, j int) { packetList[i], packetList[j] = packetList[j], packetList[i] })//Standard Shuffle
 	return packetList
+
+	//In accordance with the paper
+	// options := []Choice{{1, "no"}, {2, "yes"}}
+	// //shuffle
+	// val, err := WeightedChoice(options)
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// }
+	// if val.Item == "yes" {
+	// 	R.Shuffle(len(packetList), func(i, j int) { packetList[i], packetList[j] = packetList[j], packetList[i] })
+	// }
+	// //duplicate
+	// options = []Choice{{1, "no"}, {2, "yes"}}
+	// val, err = WeightedChoice(options)
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// }
+	// if val.Item == "yes" {
+	// 	var l []int
+	// 	for i := 0; i < len(packetList); i++ {
+	// 		if R.Float32() < 0.5 {
+	// 			l = append(l, i)
+	// 		}
+	// 	}
+	// 	for j := 0; j < len(l); j++ {
+	// 		packetList = append(packetList, packetList[j])
+	// 	}
+	// 	R.Shuffle(len(packetList), func(i, j int) { packetList[i], packetList[j] = packetList[j], packetList[i] })
+	// }
+	// //drop
+	// options = []Choice{{1, "no"}, {2, "yes"}}
+	// val, err = WeightedChoice(options)
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// }
+	// count := 0
+	// if val.Item == "yes" {
+	// 	for i := 0; i < len(packetList); i++ {
+	// 		if R.Float32() < 0.5 {
+	// 			continue
+	// 		} else {
+	// 			packetList[count] = packetList[i]
+	// 			count++
+	// 		}
+	// 	}
+	// 	packetList = packetList[:count]
+	// 	R.Shuffle(len(packetList), func(i, j int) { packetList[i], packetList[j] = packetList[j], packetList[i] })
+	// }
+	// return packetList
 }
