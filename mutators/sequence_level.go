653a670ee5051659604b95725fd16218aafaa758
piano-man <reen.gagan@gmail.com>
2020-06-09 18:29:53 +0200

    production code: mutations updated to paper

--- mutators/sequence_level.go
+++ mutators/sequence_level.go
@@ -1,61 +1,66 @@
 package mutators
 
 import (
-	// "fmt"
+	"fmt"
+
 	. "github.com/QUIC-Tracker/quic-tracker"
 )
 
 func SequenceLevelMutations(packetList []*ProtectedPacket) []*ProtectedPacket {
-	R.Shuffle(len(packetList), func(i, j int) { packetList[i], packetList[j] = packetList[i], packetList[i] }) //Modified Shuffle(Shuffle+Drop+Duplicate)
-	// // R.Shuffle(len(packetList), func(i, j int) { packetList[i], packetList[j] = packetList[j], packetList[i] })//Standard Shuffle
-	return packetList
+	// R.Shuffle(len(packetList), func(i, j int) { packetList[i], packetList[j] = packetList[i], packetList[i] }) //Modified Shuffle(Shuffle+Drop+Duplicate)
+	// // // R.Shuffle(len(packetList), func(i, j int) { packetList[i], packetList[j] = packetList[j], packetList[i] })//Standard Shuffle
+	// return packetList
 
 	//In accordance with the paper
-	// options := []Choice{{1, "no"}, {2, "yes"}}
-	// //shuffle
-	// val, err := WeightedChoice(options)
-	// if err != nil {
-	// 	fmt.Println(err.Error())
-	// }
-	// if val.Item == "yes" {
-	// 	R.Shuffle(len(packetList), func(i, j int) { packetList[i], packetList[j] = packetList[j], packetList[i] })
-	// }
-	// //duplicate
-	// options = []Choice{{1, "no"}, {2, "yes"}}
-	// val, err = WeightedChoice(options)
-	// if err != nil {
-	// 	fmt.Println(err.Error())
-	// }
-	// if val.Item == "yes" {
-	// 	var l []int
-	// 	for i := 0; i < len(packetList); i++ {
-	// 		if R.Float32() < 0.5 {
-	// 			l = append(l, i)
-	// 		}
-	// 	}
-	// 	for j := 0; j < len(l); j++ {
-	// 		packetList = append(packetList, packetList[j])
-	// 	}
-	// 	R.Shuffle(len(packetList), func(i, j int) { packetList[i], packetList[j] = packetList[j], packetList[i] })
-	// }
-	// //drop
-	// options = []Choice{{1, "no"}, {2, "yes"}}
-	// val, err = WeightedChoice(options)
-	// if err != nil {
-	// 	fmt.Println(err.Error())
-	// }
-	// count := 0
-	// if val.Item == "yes" {
-	// 	for i := 0; i < len(packetList); i++ {
-	// 		if R.Float32() < 0.5 {
-	// 			continue
-	// 		} else {
-	// 			packetList[count] = packetList[i]
-	// 			count++
-	// 		}
-	// 	}
-	// 	packetList = packetList[:count]
-	// 	R.Shuffle(len(packetList), func(i, j int) { packetList[i], packetList[j] = packetList[j], packetList[i] })
-	// }
-	// return packetList
+	options := []Choice{{1, "no"}, {1, "yes"}}
+	//shuffle
+	val, err := WeightedChoice(options)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	if val.Item == "yes" {
+		R.Shuffle(len(packetList), func(i, j int) { packetList[i], packetList[j] = packetList[j], packetList[i] })
+	}
+	//duplicate
+	options = []Choice{{1, "no"}, {1, "yes"}}
+	val, err = WeightedChoice(options)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	if val.Item == "yes" {
+		var l []int
+		var d []int
+		for i := 0; i < len(packetList); i++ {
+			if R.Float32() < 0.5 {
+				l = append(l, i)
+				d = append(d, R.Intn(5)) //duplication factor
+			}
+		}
+		for j := 0; j < len(l); j++ {
+			for k := 0; k < d[j]; k++ {
+				packetList = append(packetList, packetList[j])
+			}
+		}
+		R.Shuffle(len(packetList), func(i, j int) { packetList[i], packetList[j] = packetList[j], packetList[i] })
+	}
+	//drop
+	options = []Choice{{1, "no"}, {1, "yes"}}
+	val, err = WeightedChoice(options)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	count := 0
+	if val.Item == "yes" {
+		for i := 0; i < len(packetList); i++ {
+			if R.Float32() < 0.5 {
+				continue
+			} else {
+				packetList[count] = packetList[i]
+				count++
+			}
+		}
+		packetList = packetList[:count]
+		R.Shuffle(len(packetList), func(i, j int) { packetList[i], packetList[j] = packetList[j], packetList[i] })
+	}
+	return packetList
 }
